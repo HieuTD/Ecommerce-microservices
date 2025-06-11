ff5979598744e11212e365768318d5b50a890149
HieuByn <hieutrinhduc1612@gmail.com>
2025-06-11 22:37:56 +0700

    Commit Handlers, Commands, Queries

---
+++ Services/Catalog/Catalog.Application/Handlers/UpdateProductCommandHandler.cs
@@ -0,0 +1,35 @@
+﻿using Catalog.Application.Commands;
+using Catalog.Application.Mappers;
+using Catalog.Application.Responses;
+using Catalog.Core.Entities;
+using Catalog.Core.Repositories;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalog.Application.Handlers
+{
+    public class UpdateProductCommandHandler : IRequestHandler<UpdateProductCommand, bool>
+    {
+        private readonly IProductRepository _productRepository;
+
+        public UpdateProductCommandHandler(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        public async Task<bool> Handle(UpdateProductCommand request, CancellationToken cancellationToken)
+        {
+            var productEntity = ProductMapper.Mapper.Map<Product>(request);
+            if (productEntity == null)
+            {
+                throw new ApplicationException("There is an issue with mapping while updating product");
+            }
+            var updateProduct = await _productRepository.UpdateProduct(productEntity);
+            return true;
+        }
+    }
+}
