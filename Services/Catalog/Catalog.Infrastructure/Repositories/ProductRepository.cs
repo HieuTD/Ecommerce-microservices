3bc967a4dfc32846c0d228298bd5447a12e8a3fc
HieuByn <hieutrinhduc1612@gmail.com>
2025-06-24 22:38:48 +0700

    Add pagination, filter, sort feature (Catalog); Update Repository, Handler, Controller(Catalog)

--- Services/Catalog/Catalog.Infrastructure/Repositories/ProductRepository.cs
+++ Services/Catalog/Catalog.Infrastructure/Repositories/ProductRepository.cs
@@ -1,5 +1,6 @@
 ﻿using Catalog.Core.Entities;
 using Catalog.Core.Repositories;
+using Catalog.Core.Specs;
 using Catalog.Infrastructure.Data;
 using MongoDB.Driver;
 
@@ -18,9 +19,35 @@ namespace Catalog.Infrastructure.Repositories
             return await _context.Products.Find(p => p.Id == id).FirstOrDefaultAsync();
         }
 
-        public async Task<IEnumerable<Product>> GetProducts()
+        public async Task<Pagination<Product>> GetProducts(CatalogSpecParams catalogSpecParams)
         {
-            return await _context.Products.Find(p => true).ToListAsync();
+            var builder = Builders<Product>.Filter;
+            var filtered = builder.Empty;
+
+            if (!String.IsNullOrEmpty(catalogSpecParams.Search))
+            {
+                filtered = filtered & builder.Where(p => p.Name.ToLower().Contains(catalogSpecParams.Search));
+            }
+            if (!string.IsNullOrEmpty(catalogSpecParams.BrandId))
+            {
+                var brandFiltered = builder.Eq(p => p.Brands.Id, catalogSpecParams.BrandId);
+                filtered = filtered & brandFiltered;
+            }
+            if (!string.IsNullOrEmpty(catalogSpecParams.TypeId))
+            {
+                var typeFiltered = builder.Eq(p => p.Types.Id, catalogSpecParams.TypeId);
+                filtered = filtered & typeFiltered;
+            }
+
+            var totalItems = await _context.Products.CountDocumentsAsync(filtered);
+            var data = await DataFilter(catalogSpecParams, filtered);
+
+            return new Pagination<Product>(
+                    catalogSpecParams.PageIndex,
+                    catalogSpecParams.PageSize,
+                    (int)totalItems,
+                    data
+                );
         }
 
         public async Task<IEnumerable<Product>> GetProductsByBrand(string brandName)
@@ -41,7 +68,7 @@ namespace Catalog.Infrastructure.Repositories
 
         public async Task<bool> DeleteProduct(string id)
         {
-            var deletedProduct = await _context.Products.DeleteOneAsync(p => p.Id == id);   
+            var deletedProduct = await _context.Products.DeleteOneAsync(p => p.Id == id);
             return deletedProduct.IsAcknowledged && deletedProduct.DeletedCount > 0;
         }
 
@@ -61,6 +88,30 @@ namespace Catalog.Infrastructure.Repositories
             return await _context.Types.Find(t => true).ToListAsync();
         }
 
-
+        private async Task<IReadOnlyList<Product>> DataFilter(CatalogSpecParams catalogSpecParams, FilterDefinition<Product> filtered)
+        {
+            var sortDefn = Builders<Product>.Sort.Ascending("Name");
+            if (!string.IsNullOrEmpty(catalogSpecParams.Sort))
+            {
+                switch (catalogSpecParams.Sort)
+                {
+                    case "priceAsc":
+                        sortDefn = Builders<Product>.Sort.Ascending(p => p.Price);
+                        break;
+                    case "priceDesc":
+                        sortDefn = Builders<Product>.Sort.Descending(p => p.Price);
+                        break;
+                    default:
+                        Builders<Product>.Sort.Ascending(p => p.Name);
+                        break;
+                }
+            }
+            return await _context.Products
+                .Find(filtered)
+                .Sort(sortDefn)
+                .Skip((catalogSpecParams.PageIndex - 1) * catalogSpecParams.PageSize)
+                .Limit(catalogSpecParams.PageSize)
+                .ToListAsync();
+        }
     }
 }
