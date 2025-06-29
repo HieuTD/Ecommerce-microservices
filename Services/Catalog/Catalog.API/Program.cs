a68edb9bbda9dd01cdfb3c22b4a10115e1421683
HieuByn <hieutrinhduc1612@gmail.com>
2025-06-29 12:55:23 +0700

    Using _mapper DI

--- Services/Catalog/Catalog.API/Program.cs
+++ Services/Catalog/Catalog.API/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Asp.Versioning;
 using Catalog.Application.Handlers;
+using Catalog.Application.Mappers;
 using Catalog.Core.Repositories;
 using Catalog.Infrastructure.Data;
 using Catalog.Infrastructure.Repositories;
@@ -24,7 +25,11 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "Catalog.API", Version = "v1" });});
 
 //Register AutoMapper: scan cac lop ke thua Profile
-builder.Services.AddAutoMapper(typeof(Program).Assembly);
+builder.Services.AddAutoMapper(
+    //Use it in case of using _mapper DI
+    //typeof(ProductMappingProfile).Assembly, 
+    typeof(Program).Assembly
+);
 
 //Register MediatR: Tu đong scan va đang ky cac handler (Ex: IRequestHandler, INotificationHandler) tu assembly hien tai
 var assemblies = new Assembly[]
