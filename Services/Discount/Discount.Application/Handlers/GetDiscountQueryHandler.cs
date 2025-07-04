c17d853f7b1b5fda8d4f1501860806f58e556b60
HieuTD <hieutrinhduc1612@gmail.com>
2025-07-04 17:16:14 +0700

    Add Handlers, Mapper, Queries, Commands, Protos

---
+++ Services/Discount/Discount.Application/Handlers/GetDiscountQueryHandler.cs
@@ -0,0 +1,38 @@
+﻿using Discount.Application.Queries;
+using Discount.Core.Repositories;
+using Discount.Grpc.Protos;
+using Grpc.Core;
+using MediatR;
+using Microsoft.AspNetCore.Http.HttpResults;
+
+namespace Discount.Application.Handlers
+{
+    public class GetDiscountQueryHandler : IRequestHandler<GetDiscountQuery, CouponModel>
+    {
+        private readonly IDiscountRepository _discountRepository;
+
+        public GetDiscountQueryHandler(IDiscountRepository discountRepository)
+        {
+            _discountRepository = discountRepository;
+        }
+        public async Task<CouponModel> Handle(GetDiscountQuery request, CancellationToken cancellationToken)
+        {
+            var coupon = await _discountRepository.GetDiscount(request.ProductName);
+
+            if (coupon == null)
+            {
+                throw new RpcException(new Status(StatusCode.NotFound, $"Discount for the productName = {request.ProductName} is not found"));
+            }
+
+            var couponModel = new CouponModel
+            {
+                Id = coupon.Id,
+                ProductName = coupon.ProductName,
+                Amount = coupon.Amount,
+                Description = coupon.Description
+            };
+
+            return couponModel;
+        }
+    }
+}
