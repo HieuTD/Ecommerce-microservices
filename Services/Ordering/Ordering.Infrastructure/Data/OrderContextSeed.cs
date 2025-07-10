d10e5b983d28db426a911962e7af4f683836aaed
HieuByn <hieutrinhduc1612@gmail.com>
2025-07-10 22:24:21 +0700

    Add OrderContext, OrderContextFactory, OrderContextSeed, RepositoryBase

---
+++ Services/Ordering/Ordering.Infrastructure/Data/OrderContextSeed.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Extensions.Logging;
+using Ordering.Core.Entities;
+
+namespace Ordering.Infrastructure.Data
+{
+    public class OrderContextSeed
+    {
+        public static async Task SeedAsync(OrderContext orderContext, ILogger<OrderContextSeed> logger)
+        {
+            if (!orderContext.Orders.Any())
+            {
+                orderContext.Orders.AddRange(GetOrders());
+                await orderContext.SaveChangesAsync();
+                logger.LogInformation($"Ordering Database : {typeof(OrderContext).Name} seeded");
+            }
+        }
+
+        private static IEnumerable<Order> GetOrders()
+        {
+            return new List<Order>
+            {
+                new()
+                {
+                    UserName = "Hieu",
+                    FirstName = "Hieu",
+                    LastName = "Trinh",
+                    EmailAddress = "hieu@eCommerce.net",
+                    AddressLine = "Bach Khoa",
+                    Country = "Viet Nam",
+                    TotalPrice = 750,
+                    State = "KA",
+                    ZipCode = "560001",
+
+                    CardName = "Visa",
+                    CardNumber = "1234567890123456",
+                    CreatedBy = "Rahul",
+                    Expiration = "12/25",
+                    Cvv = "123",
+                    PaymentMethod = 1,
+                    LastModifiedBy = "Hieu",
+                    LastModifiedDate = new DateTime(),
+                }
+            };
+        }
+    }
+}
