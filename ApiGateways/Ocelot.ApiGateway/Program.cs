fde4fcb3172690a21c6ea19bc02e5b9ad1ac09ec
HieuByn <hieutrinhduc1612@gmail.com>
2025-08-30 18:20:17 +0700

    Add Ocelot Schema(json); Update Program.cs; Docker

--- ApiGateways/Ocelot.ApiGateway/Program.cs
+++ ApiGateways/Ocelot.ApiGateway/Program.cs
@@ -4,18 +4,22 @@ using Ocelot.Middleware;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CorsPolicy", policy =>
+    {
+        policy.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin();
+    });
+});
 //ocelot configuration
 builder.Host.ConfigureAppConfiguration((env, config) =>
 {
     config.AddJsonFile($"ocelot.{env.HostingEnvironment.EnvironmentName}.json", true, true);
 });
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
 builder.Services.AddOcelot();
 
 var app = builder.Build();
@@ -26,7 +30,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.UseRouting();
+app.UseCors("CorsPolicy");
 app.UseAuthorization();
 
 app.MapControllers();
@@ -37,5 +42,4 @@ app.UseEndpoints(endpoints =>
 });
 
 await app.UseOcelot();
-
 await app.RunAsync();
